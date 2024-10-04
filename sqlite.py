ebb473c9b9981ea7a71f6b179379f523ab67278e
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-10-04 10:09:48 +0200

    Refactoring

--- sqlite.py
+++ sqlite.py
@@ -1,8 +1,8 @@
 import random
 import sqlite3
-import csv
 
-create_table_clients = """
+# Constantes pour les requêtes SQL
+CREATE_TABLE_CLIENTS = """
     CREATE TABLE IF NOT EXISTS Clients (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         nom TEXT,
@@ -10,8 +10,9 @@ create_table_clients = """
         email TEXT,
         date_inscription DATE
     )
-    """
-create_table_commandes = """
+"""
+
+CREATE_TABLE_COMMANDES = """
     CREATE TABLE IF NOT EXISTS Commandes (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         client_id INTEGER,
@@ -19,22 +20,23 @@ create_table_commandes = """
         date_commande DATE,
         FOREIGN KEY (client_id) references Clients(id)
     )
-    """
-insert_client = """
+"""
+
+INSERT_CLIENT = """
 INSERT INTO Clients 
     (nom, prenom, email, date_inscription)
 VALUES
     (:nom, :prenom, :email, :date_inscription)
 """
 
-insert_commande = """
+INSERT_COMMANDE = """
 INSERT INTO Commandes 
     (client_id, produit, date_commande)
 VALUES
     (:client_id, :produit, DATE('now'))
 """
 
-get_clients = """
+GET_CLIENTS = """
 SELECT 
     id, nom, prenom, email, date_inscription
 FROM
@@ -42,22 +44,8 @@ FROM
 ORDER BY
     date_inscription DESC
 """
-select_commandes_by_client_id = """
-SELECT 
-    cm.id, cm.produit, cm.date_commande, cl.nom, cl.prenom, cl.email, cl.date_inscription
-FROM
-    Clients as cl
-LEFT JOIN 
-    Commandes as cm
-ON
-    cl.id = cm.client_id
-GROUP BY
-    cl.id
-ORDER BY
-    date_commande DESC
-"""
 
-get_commandes_by_client_id = """
+GET_COMMANDES_BY_CLIENT_ID = """
 SELECT 
     cm.id, cm.produit, cm.date_commande, cl.nom, cl.prenom, cl.email, cl.date_inscription, cl.id
 FROM
@@ -72,7 +60,7 @@ ORDER BY
     cm.date_commande DESC
 """
 
-update_client_email = """
+UPDATE_CLIENT_EMAIL = """
 UPDATE 
     Clients
 SET
@@ -81,104 +69,101 @@ WHERE
     id = :client_id
 """
 
-count_commandes = """
+COUNT_COMMANDES = """
 SELECT 
     Count(id)
 FROM
     Commandes
 """
 
-delete_commande = """
+DELETE_COMMANDE = """
 DELETE FROM
     Commandes 
 WHERE
     id = :commande_id
 """
 
-get_clients_commandes = """
-SELECT 
-    cm.id, cm.produit, cm.date_commande, cl.nom, cl.prenom, cl.email, cl.date_inscription, cl.id
-FROM
-    Clients as cl
-LEFT JOIN 
-    Commandes as cm
-ON
-    cl.id = cm.client_id
-ORDER BY
-    cl.id, date_commande DESC
-"""
+
+def create_tables(cursor):
+    cursor.execute(CREATE_TABLE_CLIENTS)
+    cursor.execute(CREATE_TABLE_COMMANDES)
 
 
-
-client_1 = {
-    "nom" : "NOM1",
-    "prenom" : "PRENOM1",
-    "email" : "nom1.prenom1@ici.la",
-    "date_inscription" : "2020-09-01"
-}
-
-client_2 = {
-    "nom" : "NOM2",
-    "prenom" : "PRENOM2",
-    "email" : "nom2.prenom2@ici.la",
-    "date_inscription" : "2023-10-01"
-}
-
-clients = [client_1, client_2]
+def insert_client(cursor, client_data):
+    cursor.execute(INSERT_CLIENT, client_data)
+    return cursor.lastrowid
 
 
+def insert_commandes(cursor, client_id, produits):
+    for produit in produits:
+        commande_data = {"client_id": client_id, "produit": produit}
+        cursor.execute(INSERT_COMMANDE, commande_data)
+    return cursor.lastrowid
+
+
+def fetch_all_clients(cursor):
+    cursor.execute(GET_CLIENTS)
+    return cursor.fetchall()
+
+
+def fetch_commandes_by_client_id(cursor, client_id):
+    cursor.execute(GET_COMMANDES_BY_CLIENT_ID, {"client_id": client_id})
+    return cursor.fetchall()
+
+
+def update_email(cursor, client_id):
+    cursor.execute(UPDATE_CLIENT_EMAIL, {"client_id": client_id})
+
+
+def fetch_commandes_count(cursor):
+    cursor.execute(COUNT_COMMANDES)
+    return cursor.fetchone()[0]
+
+
+def delete_commande(cursor, commande_id):
+    cursor.execute(DELETE_COMMANDE, {"commande_id": commande_id})
+
+
+# Initialisation de la base de données
 db_connect = sqlite3.connect('my_db.db')
 db_cursor = db_connect.cursor()
 
-db_cursor.execute(create_table_clients)
-db_cursor.execute(create_table_commandes)
+# Création des tables
+create_tables(db_cursor)
 
-for client in clients:
-    db_cursor.execute(insert_client, client)
-    db_connect.commit()
-    last_client_id = db_cursor.lastrowid
-    print('last client id = ' + str(last_client_id))
-    commande1 = {
-        "client_id" : last_client_id,
-        "produit" : "produit" + str(last_client_id),
-    }
-    commande2 = {
-        "client_id": last_client_id,
-        "produit": "produit" + str(last_client_id + 1),
-    }
-    commandes = [commande1, commande2]
-    for commande in commandes:
-        db_cursor.execute(insert_commande, commande)
-    db_connect.commit()
-    last_commande_id = db_cursor.lastrowid
-    print('last commande id = ' + str(last_commande_id))
+clients_data = [
+    {"nom": "NOM1", "prenom": "PRENOM1", "email": "nom1.prenom1@ici.la", "date_inscription": "2020-09-01"},
+    {"nom": "NOM2", "prenom": "PRENOM2", "email": "nom2.prenom2@ici.la", "date_inscription": "2023-10-01"}
+]
 
-db_cursor.execute(get_clients)
-clients = db_cursor.fetchall()
+# Insertion de clients et commandes
+for client in clients_data:
+    last_client_id = insert_client(db_cursor, client)
+    produits = [f"produit{last_client_id}", f"produit{last_client_id + 1}"]
+    last_commande_id = insert_commandes(db_cursor, last_client_id, produits)
+    db_connect.commit()
+
+# Affichage des clients
+clients = fetch_all_clients(db_cursor)
 for client in clients:
     print(client)
 
-nb_clients = int(len(clients))
+# Sélection et mise à jour d'un client aléatoire
+nb_clients = len(clients)
 random_client_id = random.randint(1, nb_clients)
 print('random client id = ' + str(random_client_id))
-db_cursor.execute(get_commandes_by_client_id, {"client_id": random_client_id})
-commandes = db_cursor.fetchall()
+commandes = fetch_commandes_by_client_id(db_cursor, random_client_id)
 for commande in commandes:
     print(commande)
-db_cursor.execute(update_client_email, {"client_id": random_client_id})
+
+update_email(db_cursor, random_client_id)
 db_connect.commit()
-db_cursor.execute(count_commandes)
-nb_commandes = db_cursor.fetchone()[0]
+
+# Suppression d'une commande aléatoire
+nb_commandes = fetch_commandes_count(db_cursor)
 random_commande_id = random.randint(1, nb_commandes)
-db_cursor.execute(delete_commande, {"commande_id": random_commande_id})
+delete_commande(db_cursor, random_commande_id)
 print('delete commande with id = ' + str(random_commande_id))
+
 db_connect.commit()
-
-
-
-
-
 db_connect.close()
-
-
-
